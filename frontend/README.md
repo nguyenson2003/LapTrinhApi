00bdfa48db582fc6dcc7ac531fb76ad66f342c9b
nqson <nqson123@gmail.com>
2024-04-30 03:08:25 +0700

    front end

---
+++ frontend/README.md
@@ -0,0 +1,11 @@
+Cách mở project:
+* Bước 1: Cài nodejs: https://nodejs.org/en
+* Bước 2: Mở terminal gõ lệnh mở folder
+```
+cd frontend
+cd my-app
+```
+* Bước 3: Chạy web
+```
+npm start
+```
---
+++ frontend/my-app/src/Nav.js
@@ -0,0 +1,109 @@
+export default function Nav() {
+    return <div data-v-2e5fb307 id="header">
+        <ul data-v-2e5fb307 class="oj-menu ivu-menu ivu-menu-light ivu-menu-horizontal">
+            <div data-v-2e5fb307 class="logo"><span>
+                <font style={{ verticalAlign: 'inherit' }}>
+                    <font style={{ verticalAlign: 'inherit' }}>QQ</font>
+                </font>
+            </span></div>
+            <li data-v-2e5fb307 class="ivu-menu-item"><i class="ivu-icon ivu-icon-home"></i>
+                <font style={{ verticalAlign: 'inherit' }}>
+                    <font style={{ verticalAlign: 'inherit' }}>
+                        Trang chủ
+                    </font>
+                </font>
+            </li>
+            <li data-v-2e5fb307 class="ivu-menu-item ivu-menu-item-active ivu-menu-item-selected"><i
+                class="ivu-icon ivu-icon-ios-keypad"></i>
+                <font style={{ verticalAlign: 'inherit' }}>
+                    <font style={{ verticalAlign: 'inherit' }}>
+                        Các vấn đề
+                    </font>
+                </font>
+            </li>
+            <li data-v-2e5fb307 class="ivu-menu-item"><i class="ivu-icon ivu-icon-trophy"></i>
+                <font style={{ verticalAlign: 'inherit' }}>
+                    <font style={{ verticalAlign: 'inherit' }}>
+                        Cuộc thi
+                    </font>
+                </font>
+            </li>
+            <li data-v-2e5fb307 class="ivu-menu-item"><i
+                class="ivu-icon ivu-icon-ios-pulse-strong"></i>
+                <font style={{ verticalAlign: 'inherit' }}>
+                    <font style={{ verticalAlign: 'inherit' }}>
+                        Trạng thái
+                    </font>
+                </font>
+            </li>
+            <li data-v-2e5fb307 class="ivu-menu-submenu">
+                <div class="ivu-menu-submenu-title"><i class="ivu-icon ivu-icon-podium"></i>
+                    <font style={{ verticalAlign: 'inherit' }}>
+                        <font style={{ verticalAlign: 'inherit' }}>
+                            Thứ hạng
+                        </font>
+                    </font><i class="ivu-icon ivu-icon-ios-arrow-down ivu-menu-submenu-title-icon"></i>
+                </div>
+                <div class="ivu-select-dropdown" style={{ 'display': 'none' }}>
+                    <ul class="ivu-menu-drop-list">
+                        <li class="ivu-menu-item">
+                            <font style={{ verticalAlign: 'inherit' }}>
+                                <font style={{ verticalAlign: 'inherit' }}>
+                                    Xếp hạng ACM
+                                </font>
+                            </font>
+                        </li>
+                        <li class="ivu-menu-item">
+                            <font style={{ verticalAlign: 'inherit' }}>
+                                <font style={{ verticalAlign: 'inherit' }}>
+                                    Thứ hạng OI
+                                </font>
+                            </font>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="ivu-menu-submenu">
+                <div class="ivu-menu-submenu-title"><i
+                    class="ivu-icon ivu-icon-information-circled"></i>
+                    <font style={{ verticalAlign: 'inherit' }}>
+                        <font style={{ verticalAlign: 'inherit' }}>
+                            Về
+                        </font>
+                    </font><i class="ivu-icon ivu-icon-ios-arrow-down ivu-menu-submenu-title-icon"></i>
+                </div>
+                <div class="ivu-select-dropdown" style={{ 'display': 'none' }}>
+                    <ul class="ivu-menu-drop-list">
+                        <li class="ivu-menu-item">
+                            <font style={{ verticalAlign: 'inherit' }}>
+                                <font style={{ verticalAlign: 'inherit' }}>
+                                    Thẩm phán
+                                </font>
+                            </font>
+                        </li>
+                        <li class="ivu-menu-item">
+                            <font style={{ verticalAlign: 'inherit' }}>
+                                <font style={{ verticalAlign: 'inherit' }}>
+                                    Câu hỏi thường gặp
+                                </font>
+                            </font>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <div class="btn-menu"><button type="button"
+                class="ivu-btn ivu-btn-ghost ivu-btn-circle"><span>
+                    <font style={{ verticalAlign: 'inherit' }}>
+                        <font style={{ verticalAlign: 'inherit' }}>Đăng nhập
+                        </font>
+                    </font>
+                </span></button> <button type="button"
+                    class="ivu-btn ivu-btn-ghost ivu-btn-circle" style={{ 'margin-left': '5px' }}><span>
+                        <font style={{ verticalAlign: 'inherit' }}>
+                            <font style={{ verticalAlign: 'inherit' }}>Đăng ký
+                            </font>
+                        </font>
+                    </span></button></div>
+        </ul>
+    </div>
+}
