033545200cf65f29f4c5cd192180aa11e8be3467
nqson <nqson123@gmail.com>
2024-04-30 19:23:43 +0700

    css

--- frontend/my-app/src/component/Nav.js
+++ frontend/my-app/src/component/Nav.js
@@ -1,7 +1,8 @@
+import './Nav.css';
 export default function Nav() {
-    return <div data-v-2e5fb307 id="header">
-        <ul data-v-2e5fb307 className="oj-menu ivu-menu ivu-menu-light ivu-menu-horizontal">
-            <div data-v-2e5fb307 className="logo"><span>
+    return <div  id="header">
+        <ul  className="oj-menu ivu-menu ivu-menu-light ivu-menu-horizontal">
+            <div  className="logo"><span>
                 <font style={{ verticalAlign: 'inherit' }}>
                     <font style={{ verticalAlign: 'inherit' }}>MSC OJ</font>
                 </font>
@@ -23,8 +24,8 @@ function NavItem({ href, text, fa }) {
         window.location = href;
     }
     return (
-        <li data-v-2e5fb307 className={"ivu-menu-item " + ((path.startsWith(href) && href!=="/") || path===href ? "ivu-menu-item-active ivu-menu-item-selected" : "")} onClick={goToLink}>
-            <i data-v-2e5fb307 className={"fa-solid " + fa}></i>
+        <li  className={"ivu-menu-item " + ((path.startsWith(href) && href!=="/") || path===href ? "ivu-menu-item-active ivu-menu-item-selected" : "")} onClick={goToLink}>
+            <i  className={"fa-solid " + fa}></i>
             <font style={{ verticalAlign: 'inherit' }}>
                 {text}
             </font>
@@ -35,7 +36,7 @@ function NavItem({ href, text, fa }) {
 function NavLogin({ user }) {
     if (user == null) {
         return (
-            <div data-v-2e5fb307 className="btn-menu" >
+            <div  className="btn-menu" >
                 <button type="button" className="ivu-btn ivu-btn-ghost ivu-btn-circle">
                     <span>
                         <font style={{ verticalAlign: 'inherit' }}>
@@ -56,21 +57,21 @@ function NavLogin({ user }) {
         )
     } else {
         return (
-            <div data-v-2e5fb307="" class="drop-menu ivu-dropdown">
+            <div class="drop-menu ivu-dropdown">
                 <div class="ivu-dropdown-rel">
-                    <button data-v-2e5fb307="" type="button" class="drop-menu-title ivu-btn ivu-btn-text">
+                    <button type="button" class="drop-menu-title ivu-btn ivu-btn-text">
                         <span>
                             {user}
-                            <i data-v-2e5fb307="" class="ivu-icon ivu-icon-arrow-down-b"></i>
+                            <i class="ivu-icon ivu-icon-arrow-down-b"></i>
                         </span>
                     </button>
                 </div>
                 <div class="ivu-select-dropdown" style={{display: 'none'}}>
-                    <ul data-v-2e5fb307="" class="ivu-dropdown-menu">
-                        <li data-v-2e5fb307="" class="ivu-dropdown-item">Home</li>
-                        <li data-v-2e5fb307="" class="ivu-dropdown-item">Submissions</li>
-                        <li data-v-2e5fb307="" class="ivu-dropdown-item">Settings</li>
-                        <li data-v-2e5fb307="" class="ivu-dropdown-item ivu-dropdown-item-divided">Logout</li>
+                    <ul class="ivu-dropdown-menu">
+                        <li class="ivu-dropdown-item">Home</li>
+                        <li class="ivu-dropdown-item">Submissions</li>
+                        <li class="ivu-dropdown-item">Settings</li>
+                        <li class="ivu-dropdown-item ivu-dropdown-item-divided">Logout</li>
                     </ul>
                 </div>
             </div>
